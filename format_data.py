84488fbc5e19081d70cfcaa018997f506fc2ac10
Canthar90 <d.cieslak90@gmail.com>
2022-08-15 22:37:21 +0200

    Written first assert format_data_for_display

--- format_data.py
+++ format_data.py
@@ -1,7 +1,12 @@
 
 
-def format_data_for_display(people) -> str:
-    return "test"
+def format_data_for_display(people) -> list:
+    data = []
+    for person in people:
+        display_data =f"{person['given_name']} {person['family_name']}: {person['title']}"
+        data.append(display_data)
+        
+    return data
 
 def format_data_for_excel(people) -> str:
     return "test"
