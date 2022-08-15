715c00b07ac410679a4349e3a3b2a9379a119913
Canthar90 <d.cieslak90@gmail.com>
2022-08-15 22:28:27 +0200

    TDD in practice not passed tests written

---
+++ format_data.py
@@ -0,0 +1,7 @@
+
+
+def format_data_for_display(people) -> str:
+    return "test"
+
+def format_data_for_excel(people) -> str:
+    return "test"
---
+++ test_pytest_TDD_example.py
@@ -0,0 +1,38 @@
+
+import pytest
+
+from format_data import format_data_for_display, format_data_for_excel
+
+
+@pytest.fixture
+def example_people_data():
+    return [
+        {
+            "given_name": "Alfonsa",
+            "family_name": "Ruzi",
+            "title": "Senior Software Engeener",
+        },
+        {
+            "given_name": "Sayid",
+            "family_name": "Khan",
+            "title": "Project Manager",
+        },
+        
+    ]
+
+
+
+def test_format_data_for_display(example_people_data):
+    
+    assert format_data_for_display(example_people_data) == [
+        "Alfonsa Ruzi: Senior Software Engeener",
+        "Sayid Khan: Project Manager",
+    ]
+    
+    
+def test_format_daa_for_excel(example_people_data):
+    
+    assert format_data_for_excel(example_people_data) == ["""given,family,title
+     Alfonsa,Ruzi,Senior Software Engeener
+     Saydi,Khan,Project Manager 
+     """]
